feb16f1f10e4e4e122c6d5990d41ad75e2fd32e3
lawrencejews <lawrencejews1@gmail.com>
2021-10-01 12:21:12 +0900

    color change & res to response

--- sections/basic/main.js
+++ sections/basic/main.js
@@ -1,6 +1,6 @@
 const listRepos = async username => {
     const repos = await fetch(`https://api.github.com/users/${username}/repos?type=owner&sort=updated`)
-    .then(res => res.json())
+    .then(response => response.json())
     .catch(error => console.error(error));
     
     const markup = repos
